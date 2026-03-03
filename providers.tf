a4920cf8e862e94d943527f7e4aae298464f21ce
marucruz <75045890+marucruz@users.noreply.github.com>
2026-03-03 16:09:39 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
     bucket         = "maru-s3bucket-date"
     key            = "globalstate/s3/terraform.tfstate"
     region         = "eu-west-1"
-    use_lockfile = "maru-date-dblocks"
+    use_lockfile   = true
     encrypt        = true
   }
 }
